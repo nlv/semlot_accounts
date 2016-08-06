0c15fff2e28f03885d8cc4ad878c32a843f20d67
Leon Nikitin <nlv@lab321.ru>
2016-08-06 12:10:18 +0600

    init

---
+++ manifest.php
@@ -0,0 +1,33 @@
+<?php
+$manifest = array(
+    'name' => 'semlot_accounts',
+    'acceptable_sugar_versions' => array(),
+    'acceptable_sugar_flavors' => array('CE'),
+    'author' => 'Leon.V.Nikitin@pravmail.ru',
+    'description' => 'Доработка модуля контрагентов (semlot)',
+    'is_uninstallable' => true,
+    'published_date' => '2016-08-06',
+    'type' => 'module',
+    'version' => '1.0.0-1alpha',
+);
+$installdefs = array(
+    'id' => 'semlot_accounts',
+    'copy' => array(
+        array(
+            'from' => '<basepath>/source/copy',
+            'to' => '.'
+        ),
+    ),
+	'language' => array (
+      array (
+        'from' => '<basepath>/source/modules/application/language/ru_ru.lang.php',
+        'to_module' => 'application',
+        'language' => 'ru_ru',
+      ),
+      array (
+        'from' => '<basepath>/source/modules/Accounts/language/ru_ru.lang.php',
+        'to_module' => 'Accounts',
+        'language' => 'ru_ru',
+      ),
+	),
+);
