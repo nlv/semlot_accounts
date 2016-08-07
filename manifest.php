d50490e6defa6f6027a9fd20ce3fc956e84179c7
Leon Nikitin <nlv@lab321.ru>
2016-08-07 12:33:19 +0600

    Добавлена связь accounts_contacts2

--- manifest.php
+++ manifest.php
@@ -8,7 +8,7 @@ $manifest = array(
     'is_uninstallable' => true,
     'published_date' => '2016-08-06',
     'type' => 'module',
-    'version' => '1.0.0',
+    'version' => '1.1.0.2alpha',
 );
 $installdefs = array(
     'id' => 'semlot_accounts',
@@ -29,5 +29,38 @@ $installdefs = array(
         'to_module' => 'Accounts',
         'language' => 'ru_ru',
       ),
+      array (
+        'from' => '<basepath>/source/modules/Contacts/language/ru_ru.lang.php',
+        'to_module' => 'Contacts',
+        'language' => 'ru_ru',
+      ),
 	),
+
+  'vardefs' => array (
+      array (
+        'from' => '<basepath>/source/modules/Accounts/vardefs/vardefs.php',
+          'to_module' => 'Accounts',
+      ),
+      array (
+        'from' => '<basepath>/source/modules/Contacts/vardefs/vardefs.php',
+          'to_module' => 'Contacts',
+      ),
+  ),
+
+  'relationships' => array (
+      array (
+        'meta_data' => '<basepath>/source/relationships/accounts_contacts2MetaData.php',
+      ),
+  ),
+
+  'layoutdefs' => array (
+      array (
+        'from' => '<basepath>/source/modules/Accounts/layoutdefs/layoutdefs.php',
+          'to_module' => 'Accounts',
+      ),
+      array (
+        'from' => '<basepath>/source/modules/Contacts/layoutdefs/layoutdefs.php',
+          'to_module' => 'Contacts',
+      ),
+  ),
 );
